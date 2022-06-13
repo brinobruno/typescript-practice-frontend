5754d319b60fdb72ed37122f679354e507f0f3e2
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-06-13 18:57:30 -0300

    receive props / interface for props

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { api } from './services/api'
 
+import { User } from './components/User'
+
 interface IUser {
   name: string
   email: string
@@ -18,10 +20,10 @@ function App() {
   return (
     <>
     <div className="App">
-      { users.map((user) => <p>{ `${ user.name } = ${ user.email }` }</p>)}
+      { users.map((user) => <User key={ user.email }  user={ user } />)}
     </div>
     </>
   )
 }
 
-export default App;
+export default App
